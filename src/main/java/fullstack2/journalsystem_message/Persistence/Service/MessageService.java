b87991dfc8441d796520ff1f7a944c450ee87e82
osama251 <164792404+osama251@users.noreply.github.com>
2026-01-10 16:43:03 +0100

    Update MessageService.java

--- src/main/java/fullstack2/journalsystem_message/Persistence/Service/MessageService.java
+++ src/main/java/fullstack2/journalsystem_message/Persistence/Service/MessageService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 public class MessageService {
-
+    //...
     private final MessageRepository messageRepository;
     private final ConversationRepository conversationRepository;
 
